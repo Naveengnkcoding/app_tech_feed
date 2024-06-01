736f1430e8a042ee6ffbec5050efe796c693884d
pradeeshpgp <prabhakaranpradheesh@gmail.com>
2024-06-01 18:48:59 +0530

    login_changes

---
+++ web-app/asaf/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import React from "react";
+import Login from "./Login";
+
+export default function Home() {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+}
