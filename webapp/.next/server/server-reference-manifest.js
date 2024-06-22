7e2c2714afa7c42b440b3870ce27cfeb95bff45e
HARISH-CODER <112271304+HARISH-DEV433@users.noreply.github.com>
2024-06-22 14:20:30 +0530

    pacxkages

--- webapp/.next/server/server-reference-manifest.js
+++ webapp/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"a8eAhXp26htI/1YyyCTXVsNG+SvYlu9DeqX2TRqzOHM=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"46GdJ0zy4sRmptzhv4qtCy+ggh9UQfAB1knN18oGAQw=\"}"
