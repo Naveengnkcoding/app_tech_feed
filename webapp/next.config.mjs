b796d4b2b1a65a2faf57f9ec4fd367e53aea9b51
HARISH-CODER <112271304+HARISH-DEV433@users.noreply.github.com>
2024-06-26 18:31:05 +0530

    update

--- webapp/next.config.mjs
+++ webapp/next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-      ppr: 'incremental',
-    },
-  };
-   
-  module.exports = nextConfig;
+const nextConfig = {}
+
 export default nextConfig;
