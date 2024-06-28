41032702848b9485b01747a1f09b723841aa48b3
HARISH-CODER <112271304+HARISH-DEV433@users.noreply.github.com>
2024-06-28 19:35:10 +0530

    new

---
+++ admin/src/app/application/page.tsx
@@ -0,0 +1,42 @@
+"use client";
+import React, { useState, ReactNode } from "react";
+import next from "next";
+import Sidebar from "@/components/Sidebar";
+import Header from "@/components/Header";
+import DefaultLayout from "@/components/Layouts/DefaultLaout";
+
+export default function Applications({
+    children,
+  }: {
+    children: React.ReactNode;
+  }) {
+    const [sidebarOpen, setSidebarOpen] = useState(false);
+    return (
+      <>
+        {/* <!-- ===== Page Wrapper Star ===== --> */}
+        <div className="flex h-screen overflow-hidden">
+          {/* <!-- ===== Sidebar Star ===== --> */}
+          <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+          {/* <!-- ===== Sidebar End ===== --> */}
+  
+          {/* <!-- ===== Content Area Star ===== --> */}
+          <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+            {/* <!-- ===== Header Star ===== --> */}
+            <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+            {/* <!-- ===== Header End ===== --> */}
+  
+            {/* <!-- ===== Main Content Star ===== --> */}
+            <main>
+              <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+                {children}
+              </div>
+            </main>
+            {/* <!-- ===== Main Content End ===== --> */}
+          </div>
+          {/* <!-- ===== Content Area End ===== --> */}
+        </div>
+        {/* <!-- ===== Page Wrapper End ===== --> */}
+      </>
+    );
+  }
+  
