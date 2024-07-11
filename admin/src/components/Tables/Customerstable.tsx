9c32e75490f04f8929e0d3637169ff8d492024fe
HARISH-CODER <112271304+HARISH-DEV433@users.noreply.github.com>
2024-07-11 07:24:31 +0600

    continue

--- admin/src/components/Tables/Customerstable.tsx
+++ admin/src/components/Tables/Customerstable.tsx
@@ -89,12 +89,6 @@ const Tablecust = () => {
           </thead>
           <tbody>
             <tr className="border-b bg-white hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600">
-              {/* <td className="w-4 p-4">
-                    <div className="flex items-center">
-                        <input id="checkbox-table-search-1" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                        <label  className="sr-only">checkbox</label>
-                    </div>
-                </td> */}
               <th
                 scope="row"
                 className="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white"
